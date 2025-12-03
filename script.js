b23179ee7ae5d27e12286fe266689c179120f4b3
Yashwanth M <byg2281999@gmail.com>
2025-12-03 14:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener("click" , alerting);
+
+function alerting(){
+	const firstName = document.getElementById('first-name').value;
+	const lastName =  document.getElementById('last-name').value;
+	const phoneNumber =  document.getElementById('phone-number').value;
+	const emailId =  document.getElementById('email-id').value;
+	const alertmessage = alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
+	return alertmessage ;
+}
