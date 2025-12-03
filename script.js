06b8a7e8446f370826637671718bfaf842a98d2e
Yashwanth M <byg2281999@gmail.com>
2025-12-03 14:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.getElementById('submit').addEventListener("click", alerting);
-
-function alerting(){
-    const firstName = document.getElementById('first-name').value;
-    const lastName =  document.getElementById('last-name').value;
-    const phoneNumber =  document.getElementById('phone-number').value;
-    const emailId =  document.getElementById('email-id').value;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('submit').addEventListener("click", alerting);
     
-    // Just show alert, don't store or return it
-    alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
-}
+    function alerting() {
+        const firstName = document.getElementById('first-name').value;
+        const lastName = document.getElementById('last-name').value;
+        const phoneNumber = document.getElementById('phone-number').value;
+        const emailId = document.getElementById('email-id').value;
+        
+        alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
+    }
+});
