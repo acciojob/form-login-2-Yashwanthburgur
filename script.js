40ec4e4dcc87297e6a6781cb5d3b8dc117338190
Yashwanth M <byg2281999@gmail.com>
2025-12-03 14:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your JS code here. If required.
-document.getElementById('submit').addEventListener("click" , alerting);
+document.getElementById('submit').addEventListener("click", alerting);
 
 function alerting(){
-	const firstName = document.getElementById('first-name').value;
-	const lastName =  document.getElementById('last-name').value;
-	const phoneNumber =  document.getElementById('phone-number').value;
-	const emailId =  document.getElementById('email-id').value;
-	const alertmessage = alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
-	return alertmessage ;
+    const firstName = document.getElementById('first-name').value;
+    const lastName =  document.getElementById('last-name').value;
+    const phoneNumber =  document.getElementById('phone-number').value;
+    const emailId =  document.getElementById('email-id').value;
+    
+    // Just show alert, don't store or return it
+    alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
 }
