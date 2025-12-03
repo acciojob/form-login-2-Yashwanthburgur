92215c8c552de93ce709fb972535cb45401d1b2c
Yashwanth M <byg2281999@gmail.com>
2025-12-03 14:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function showAlert() {
+document.getElementById('submit').addEventListener('click', alerting);
+
+function alerting() {
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
     const phoneNumber = document.getElementById('phone-number').value;
     const emailId = document.getElementById('email-id').value;
-    
+
     alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
 }
-
-document.getElementById('submit').addEventListener('click', showAlert);
